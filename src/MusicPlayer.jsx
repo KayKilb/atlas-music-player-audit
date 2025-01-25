2779031faa646bb753794e42a8ff335210be8fa8
KayKilb <115676011+KayKilb@users.noreply.github.com>
2025-01-24 19:29:29 -0600

    updated music players layout to remove all the space

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -4,14 +4,14 @@ import Playlist from './components/Playlist';
 
 const MusicPlayer = () => {
   return (
-    <div className="music-player flex flex-col md:flex-row justify-between gap-8">
+    <div className="music-player flex flex-col md:flex-row justify-center md:justify-between items-center md:items-start gap-8 p-4">
       {/* Currently Playing Section */}
-      <div className="w-[426px] h-[1043px] md:w-[436px] md:h-[662px] md:top-[-1435px] md:left-[820px]">
+      <div className="flex-1 max-w-md md:max-w-lg">
         <CurrentlyPlaying />
       </div>
 
       {/* Playlist Section */}
-      <div className="w-[426px] h-[1043px] md:w-[481px] md:h-[662px]">
+      <div className="flex-1 max-w-md md:max-w-lg">
         <Playlist />
       </div>
     </div>
