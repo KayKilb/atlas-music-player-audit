22cecd01f7e22f8ec9bf1915a5b6c46e363334c4
KayKilb <115676011+KayKilb@users.noreply.github.com>
2025-01-28 13:56:55 -0600

    padding is way off

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -4,14 +4,14 @@ import Playlist from './components/Playlist';
 
 const MusicPlayer = () => {
   return (
-    <div className="music-player flex flex-col md:flex-row justify-center md:justify-between items-center md:items-start gap-8 p-4">
+    <div className="music-player flex flex-col md:flex-row justify-center md:justify-between items-start gap-8 p-4 bg-pink-50 h-screen">
       {/* Currently Playing Section */}
       <div className="flex-1 max-w-md md:max-w-lg">
         <CurrentlyPlaying />
       </div>
 
       {/* Playlist Section */}
-      <div className="flex-1 max-w-md md:max-w-lg">
+      <div className="flex-1 max-w-md md:max-w-lg overflow-y-auto">
         <Playlist />
       </div>
     </div>
